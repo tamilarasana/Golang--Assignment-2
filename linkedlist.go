8d0a4ed2dc079741199fe0de8f0a28ec99a4f253
tamilarasana <tamilarsanars@gmail.com>
2020-11-19 18:50:42 +0530

    Linkedlist

---
+++ linkedlist.go
@@ -0,0 +1,44 @@
+package main
+
+
+import "fmt"
+
+type node struct{
+
+	data int
+	next *node
+}
+
+func insert	(head*node, data int )*node{
+	n := &node{data:data}
+
+	if  head == nil{
+		return n 
+	}else{
+		n.next = head
+		return n
+	}
+}
+
+func Printlist(head *node){
+	for head != nil {
+		fmt.Println(head.data, "->")
+		head =head.next
+	}
+
+	fmt.Println(nil)
+}
+
+func main(){
+	var link *node
+
+	link = insert(link,1)
+	link = insert(link,2)
+	link = insert(link,3)
+	link = insert(link,4)
+	link = insert(link,5)
+	link = insert(link,6)
+	link = insert(link,7)
+
+	Printlist(link)
+}
